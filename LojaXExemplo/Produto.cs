b5cda2afe182f55d6333d18f7ec350ceb2bb5400
HeroRicky_GAMES <ricojn9@gmail.com>
2024-01-15 17:14:19 -0300

    Adição do produto em uma classe separada

---
+++ LojaXExemplo/Produto.cs
@@ -0,0 +1,15 @@
+﻿
+//Programado por HeroRickyGames
+
+namespace LojaXExemplo {
+    internal class Produto {
+        public String Nome;
+        public double Preco;
+        public int Quantidade;
+
+        public double ValorTotalEmEstoque() {
+
+            return Preco * Quantidade;
+        }
+    }
+}
---
+++ LojaXExemplo/Program.cs
@@ -0,0 +1,25 @@
+﻿
+//Programado por HeroRickyGames
+
+using System.Globalization;
+
+namespace LojaXExemplo {
+    class program {
+        static void Main(string[] args) {
+            Produto p = new Produto();
+
+            Console.WriteLine("//Programado por HeroRickyGames!");
+            Console.WriteLine("Entre os dados do produto:");
+            Console.Write("Nome: ");
+            p.Nome = Console.ReadLine();
+            Console.Write("Preço: ");
+
+            p.Preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.Write("Quantidade em Estoque: ");
+            p.Quantidade = int.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("Dados do produto: " + p);
+        }
+    }
+}
